8fb3f45571b1ea7ef8a514f2aa8b904d7144a429
maneatingape <44142177+maneatingape@users.noreply.github.com>
2024-12-01 09:15:01 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+macro_rules! wrapper {
+    ($outer:tt, $inner:tt) => {
+        pub mod $outer {
+            use aoc::year2024::$inner::{parse, part1 as p1, part2 as p2};
+            use std::fmt::Display;
+
+            pub fn part1(input: &str) -> impl Display {
+                p1(&parse(input))
+            }
+
+            pub fn part2(input: &str) -> impl Display {
+                p2(&parse(input))
+            }
+        }
+    };
+}
