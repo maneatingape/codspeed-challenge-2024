54c0ddbd8cd02605eb52bc5b47897383303add96
maneatingape <44142177+maneatingape@users.noreply.github.com>
2024-12-01 09:15:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Codspeed
+
+Library wrapper around [advent-of-code-rust](https://github.com/maneatingape/advent-of-code-rust)
+for the [Advent of Codspeed](https://codspeed.io/advent) performance challenge.
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+macro_rules! wrapper {
+    ($outer:tt, $inner:tt) => {
+        pub mod $outer {
+            use aoc::year2024::$inner::{parse, part1 as p1, part2 as p2};
+
+            #[inline]
+            pub fn part1(input: &str) -> String {
+                p1(&parse(input)).to_string()
+            }
+
+            #[inline]
+            pub fn part2(input: &str) -> String {
+                p2(&parse(input)).to_string()
+            }
+        }
+    };
+}
