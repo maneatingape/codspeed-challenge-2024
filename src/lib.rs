be679fc4ce35fb787974057582cde9cea60fe28b
maneatingape <44142177+maneatingape@users.noreply.github.com>
2024-12-18 19:44:00 +0100

    Concrete return type

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,15 @@ macro_rules! wrapper {
     ($outer:tt, $inner:tt) => {
         pub mod $outer {
             use aoc::year2024::$inner::{parse, part1 as p1, part2 as p2};
-            use std::fmt::Display;
 
-            pub fn part1(input: &str) -> impl Display {
-                p1(&parse(input))
+            #[inline]
+            pub fn part1(input: &str) -> String {
+                p1(&parse(input)).to_string()
             }
 
-            pub fn part2(input: &str) -> impl Display {
-                p2(&parse(input))
+            #[inline]
+            pub fn part2(input: &str) -> String {
+                p2(&parse(input)).to_string()
             }
         }
     };
